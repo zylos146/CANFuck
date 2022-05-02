c0c709e40e39b1141dd96c08049552356f54a87f
Avery <avery@centralnex.us>
2022-05-02 15:55:47 -0400

    Code compiles, untested

--- lib/CANOpenNode/src/CO_driver.c
+++ lib/CANOpenNode/src/CO_driver.c
@@ -30,17 +30,19 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "esp_err.h"
 #include "esp_log.h"
-#include <stdlib.h>
-#include <stdint.h>
-#include "driver/can.h"
 #include "esp_timer.h"
+
 #include "soc/soc.h"
+
 #include "CANopen.h"
 #include "CO_OD.h"
 #include "CO_config.h"
@@ -48,29 +50,25 @@
 #include "CO_driver.h"
 #include "CO_Emergency.h"
 #include "CO_config.h"
-#include "stdio.h"
-#include <stdlib.h>
-#include <stdint.h>
-#include "esp_err.h"
-#include "esp_log.h"
+
+#include "driver/twai.h"
 #include "driver/gpio.h"
-#include "driver/can.h"
 
 CO_CANmodule_t *CANmodulePointer = NULL;
 
 //CAN Timing configuration
-static can_timing_config_t timingConfig = CAN_TIMING_CONFIG_1MBITS();     //Set Baudrate to 1Mbit
+static twai_timing_config_t timingConfig = TWAI_TIMING_CONFIG_1MBITS();     //Set Baudrate to 1Mbit
                                                                           //CAN Filter configuration
-static can_filter_config_t filterConfig = CAN_FILTER_CONFIG_ACCEPT_ALL(); //Disable Message Filter
+static twai_filter_config_t filterConfig = TWAI_FILTER_CONFIG_ACCEPT_ALL(); //Disable Message Filter
                                                                           //CAN General configuration
-static can_general_config_t generalConfig = {.mode = CAN_MODE_NORMAL,
+static twai_general_config_t generalConfig = {.mode = TWAI_MODE_NORMAL,
                                              .tx_io = CAN_TX_IO,                  /*TX IO Pin (CO_config.h)*/
                                              .rx_io = CAN_RX_IO,                  /*RX IO Pin (CO_config.h)*/
-                                             .clkout_io = CAN_IO_UNUSED,          /*No clockout pin*/
-                                             .bus_off_io = CAN_IO_UNUSED,         /*No busoff pin*/
+                                             .clkout_io = TWAI_IO_UNUSED,          /*No clockout pin*/
+                                             .bus_off_io = TWAI_IO_UNUSED,         /*No busoff pin*/
                                              .tx_queue_len = CAN_TX_QUEUE_LENGTH, /*ESP TX Buffer Size (CO_config.h)*/
                                              .rx_queue_len = CAN_RX_QUEUE_LENGTH, /*ESP RX Buffer Size (CO_config.h)*/
-                                             .alerts_enabled = CAN_ALERT_NONE,    /*Disable CAN Alarms TODO: Enable for CO_CANverifyErrors*/
+                                             .alerts_enabled = TWAI_ALERT_NONE,    /*Disable CAN Alarms TODO: Enable for CO_CANverifyErrors*/
                                              .clkout_divider = 0};                /*No Clockout*/
 
 //Timer Interrupt Configuration
@@ -92,9 +90,9 @@ void CO_CANsetConfigurationMode(void *CANdriverState)
 void CO_CANsetNormalMode(CO_CANmodule_t *CANmodule)
 {
     /*Install CAN driver*/
-    ESP_ERROR_CHECK(can_driver_install(&generalConfig, &timingConfig, &filterConfig));
+    ESP_ERROR_CHECK(twai_driver_install(&generalConfig, &timingConfig, &filterConfig));
     /*Start CAN Controller*/
-    ESP_ERROR_CHECK(can_start());
+    ESP_ERROR_CHECK(twai_start());
 
     /*WORKAROUND: INTERRUPT ALLOCATION FÜR CAN NICHT MÖGLICH DA BEREITS IM IDF TREIBER VERWENDET*/
     /* Configure Timer interrupt function for execution every CO_CAN_PSEUDO_INTERRUPT_INTERVAL */
@@ -155,14 +153,14 @@ CO_ReturnError_t CO_CANmodule_init(
     }
 
     /* Configure CAN module registers */
-    generalConfig.mode = CAN_MODE_NORMAL;
+    generalConfig.mode = TWAI_MODE_NORMAL;
     generalConfig.tx_io = CAN_TX_IO;
     generalConfig.rx_io = CAN_RX_IO;
-    generalConfig.clkout_io = CAN_IO_UNUSED;
-    generalConfig.bus_off_io = CAN_IO_UNUSED;
+    generalConfig.clkout_io = TWAI_IO_UNUSED;
+    generalConfig.bus_off_io = TWAI_IO_UNUSED;
     generalConfig.tx_queue_len = CAN_TX_QUEUE_LENGTH;
     generalConfig.rx_queue_len = CAN_RX_QUEUE_LENGTH;
-    generalConfig.alerts_enabled = CAN_ALERT_ALL;
+    generalConfig.alerts_enabled = TWAI_ALERT_ALL;
     generalConfig.clkout_divider = 0;
 
     /* Configure CAN timing */
@@ -273,7 +271,7 @@ CO_ReturnError_t CO_CANmodule_init(
 void CO_CANmodule_disable(CO_CANmodule_t *CANmodule)
 {
     /* turn off the module */
-    ESP_ERROR_CHECK(can_stop());
+    ESP_ERROR_CHECK(twai_stop());
 }
 
 /******************************************************************************/
@@ -362,8 +360,8 @@ CO_CANtx_t *CO_CANtxBufferInit(
 CO_ReturnError_t CO_CANsend(CO_CANmodule_t *CANmodule, CO_CANtx_t *buffer)
 {
     CO_ReturnError_t err = CO_ERROR_NO;
-    can_status_info_t esp_can_hw_status;                      /* Define variable for hardware status of esp can interface */
-    ESP_ERROR_CHECK(can_get_status_info(&esp_can_hw_status)); /* Get hardware status of esp can interface */
+    twai_status_info_t esp_can_hw_status;                      /* Define variable for hardware status of esp can interface */
+    ESP_ERROR_CHECK(twai_get_status_info(&esp_can_hw_status)); /* Get hardware status of esp can interface */
 
     /* Verify overflow */
     if (buffer->bufferFull)
@@ -382,22 +380,22 @@ CO_ReturnError_t CO_CANsend(CO_CANmodule_t *CANmodule, CO_CANtx_t *buffer)
     if ((esp_can_hw_status.msgs_to_tx < CAN_TX_QUEUE_LENGTH) && (CANmodule->CANtxCount == 0))
     {
         CANmodule->bufferInhibitFlag = buffer->syncFlag;
-        can_message_t temp_can_message; /* generate esp can message for transmission */
+        twai_message_t temp_can_message; /* generate esp can message for transmission */
         /*MESSAGE MIT DATEN FÜLLEN*/
         temp_can_message.identifier = buffer->ident;     /* Set message-id in esp can message */
         temp_can_message.data_length_code = buffer->DLC; /* Set data length in esp can message */
-        temp_can_message.flags = CAN_MSG_FLAG_NONE;      /* reset all flags in esp can message */
-        //temp_can_message.flags = CAN_MSG_FLAG_SELF;
+        temp_can_message.flags = TWAI_MSG_FLAG_NONE;      /* reset all flags in esp can message */
+        //temp_can_message.flags = TWAI_MSG_FLAG_SELF;
         if (buffer->rtr)
         {
-            temp_can_message.flags |= CAN_MSG_FLAG_RTR; /* set rtr-flag if needed */
+            temp_can_message.flags |= TWAI_MSG_FLAG_RTR; /* set rtr-flag if needed */
         }
         for (uint8_t i = 0; i < buffer->DLC; i++)
         {
             temp_can_message.data[i] = buffer->data[i]; /* copy data from buffer in esp can message */
         }
         /* Transmit esp can message.  */
-        if (can_transmit(&temp_can_message, pdMS_TO_TICKS(CAN_TICKS_TO_WAIT)) == ESP_OK)
+        if (twai_transmit(&temp_can_message, pdMS_TO_TICKS(CAN_TICKS_TO_WAIT)) == ESP_OK)
         {
             ESP_LOGI("CANsend", "ID: %d , Data %d,%d,%d,%d,%d,%d", temp_can_message.identifier, temp_can_message.data[0], temp_can_message.data[1], temp_can_message.data[2], temp_can_message.data[3], temp_can_message.data[4], temp_can_message.data[5]);
         }
@@ -545,13 +543,13 @@ void CO_CANverifyErrors(CO_CANmodule_t *CANmodule)
 void CO_CANinterrupt(void *args)
 {
     CO_CANmodule_t *CANmodule = CANmodulePointer;
-    can_status_info_t esp_can_hw_status;                      /* Define variable for hardware status of esp can interface */
-    ESP_ERROR_CHECK(can_get_status_info(&esp_can_hw_status)); /* Get hardware status of esp can interface */
+    twai_status_info_t esp_can_hw_status;                      /* Define variable for hardware status of esp can interface */
+    ESP_ERROR_CHECK(twai_get_status_info(&esp_can_hw_status)); /* Get hardware status of esp can interface */
     /* receive interrupt */
     if (esp_can_hw_status.msgs_to_rx != 0)
     {
-        can_message_t temp_can_message; //ESP data type can message
-        ESP_ERROR_CHECK(can_receive(&temp_can_message, pdMS_TO_TICKS(CAN_TICKS_TO_WAIT)));
+        twai_message_t temp_can_message; //ESP data type can message
+        ESP_ERROR_CHECK(twai_receive(&temp_can_message, pdMS_TO_TICKS(CAN_TICKS_TO_WAIT)));
 
         CO_CANrxMsg_t rcvMsg;      /* pointer to received message in CAN module */
         uint16_t index;            /* index of received message */
@@ -561,7 +559,7 @@ void CO_CANinterrupt(void *args)
 
         rcvMsg.ident = temp_can_message.identifier;
         /* check if rtr flag is set in esp can message*/
-        if (temp_can_message.flags & CAN_MSG_FLAG_RTR)
+        if (temp_can_message.flags & TWAI_MSG_FLAG_RTR)
         {
             rcvMsg.ident += (1 << 12); /* Set RTR flag in library message */
         }
